b5adb858062ef46c047b7a70c5cdbacfc3238f32
oboyle3 <po720999@sju.edu>
2025-05-08 22:32:56 -0400

    connected postgresql db

--- caddie_universe/settings.py
+++ caddie_universe/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'caddie_universe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'caddie_universe_db',       # your DB name
+        'USER': 'postgres',                 # or your custom username
+        'PASSWORD': 'Larrybird33',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
