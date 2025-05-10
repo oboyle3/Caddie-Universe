603415794e9f10c0fa1ab265742379e5fdc2949e
oboyle3 <po720999@sju.edu>
2025-05-09 22:15:27 -0400

    added ListOfCaddie model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
 # Create your models here.
+# lets create a ListOfCaddies model
+class ListOfCaddies(models.Model):
+    name = models.CharField(max_length=100)
+    available = models.BooleanField(default=True)
+    def __str__(self):
+        #when shown in admin youll see "name" then avalibiliy
+        return f"{self.name} {self.available}"
+
+
+
+
+
+  
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,19 @@
 # core/urls.py
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing'),
+    path('admin/', admin.site.urls),
 ]
+
+
+# from django.contrib import admin
+# from django.urls import path
+# from app.views import index
+# from app import views
+# from app.views import PlayerListAPIView
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
