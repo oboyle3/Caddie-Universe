36ad2b50c337ca838e61e4fe42c4d9127993f994
oboyle3 <po720999@sju.edu>
2025-05-10 22:20:37 -0400

    chge

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def landing_page(request):
     return render(request, "core/landing.html")
+
+def personal_profile_page(request):
+    return render (request, "core/personal.html")
