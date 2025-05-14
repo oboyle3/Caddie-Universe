66621359fa59f79252bd74c14f234a76e610d10f
oboyle3 <po720999@sju.edu>
2025-05-14 19:43:08 -0400

    added auser screen

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing'),
-    path('admin/', admin.site.urls),
-    path('profile/', views.personal_profile_page, name='personal_profile')
+    
 ]
 
 
--- core/views.py
+++ core/views.py
@@ -3,9 +3,3 @@ from .models import ListOfCaddies
 # Create your views here.
 def landing_page(request):
     return render(request, "core/landing.html")
-
-
-
-def personal_profile_page(request):
-    caddies = ListOfCaddies.objects.all()
-    return render(request, "core/personal_user_page.html", {"caddies": caddies})
