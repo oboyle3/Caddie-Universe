dee5deb9a06665e82e421f418bd179775a23e48b
oboyle3 <po720999@sju.edu>
2025-05-19 21:11:45 -0400

    urls

--- core/urls.py
+++ core/urls.py
@@ -7,17 +7,5 @@ urlpatterns = [
     path('', views.landing_page, name='landing'),
     path('profile/', views.personal_profile, name='personal_profile'),
     path('booking/', views.booking, name='booking'),
-
-
     
 ]
-
-
-# from django.contrib import admin
-# from django.urls import path
-# from app.views import index
-# from app import views
-# from app.views import PlayerListAPIView
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
